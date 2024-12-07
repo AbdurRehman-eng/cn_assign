07545aca2337957c665d86fdb4629662551f1b3e
Abd Ur Rehman Shafique <shafiqueabdurrehman1@gmail.com>
2024-12-07 17:17:20 +0500

    update

--- client.py
+++ client.py
@@ -1,20 +1,24 @@
 import socket
 
-def recvFile ( file_path, host = "127.0.0.1", port = 5000):
+def recvFile ( fileName, host = "127.0.0.1", port = 5000):
     
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
 
     print("connection successful!.")
+    fileExten = s.recv(1024).decode()
 
-    with open(file_path, "wb") as file:
+    file_name = fileName + fileExten
+
+    with open(file_name, "wb") as file:
         while True:
             frag = s.recv(1024)
             if not frag:
                 break
             file.write(frag)
     
-    print(f"File received and saved to {file_path}")
+    print(f"File received and saved as {file_name}")
     s.close()
 
-recvFile("ne.tif")
+fileName = input("Enter the name of the file(without extension): ")
+recvFile(fileName)
--- server.py
+++ server.py
@@ -9,7 +9,13 @@ def sendFile ( file_path, host = '0.0.0.0', port = 5000 ):
     s.listen(1)
     print(f"Server listening on Host: {host} and Port: {port}")
     con, addr = s.accept()
+    print(f"Sending file to {addr}")
+
+    sep = "."
+
+    fileExten = file_path[file_path.rfind(sep):]
     with open(file_path, "rb") as file:
+        con.sendall(fileExten.encode())
         while fragment := file.read(1024):
             con.sendall(fragment)
     print("File sent successfully.")
